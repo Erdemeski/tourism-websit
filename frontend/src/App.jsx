ec754a3dd34949038112a7d69893a50eba445e82
Erdem Eşki <erdemeski00@gmail.com>
2024-10-11 00:44:27 +0300

    routes for first pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React from 'react'
+import HomePage from './pages/HomePage.jsx'
+import AboutPage from './pages/AboutPage.jsx'
+import SignInPage from './pages/SignInPage.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import DashboardPage from './pages/DashboardPage.jsx'
+import AdvertisementsPage from './pages/AdvertisementsPage.jsx'
 
 export default function App() {
   return (
-    <h1>App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/advertisements" element={<AdvertisementsPage />} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ frontend/src/pages/AdvertisementsPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AdvertisementsPage() {
+  return (
+    <div>AdvertisementsPage</div>
+  )
+}
