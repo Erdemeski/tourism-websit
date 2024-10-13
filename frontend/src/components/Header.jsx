05886f586eb40fedefca23e2248ce6ef4586abca
Erdem Eşki <erdemeski00@gmail.com>
2024-10-13 14:10:00 +0300

    banckend part

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,6 +3,9 @@ import { Link, useLocation } from 'react-router-dom'
 import React from 'react'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { FaMoon } from 'react-icons/fa'
+import { GrCurrency } from "react-icons/gr"
+import { MdLanguage } from "react-icons/md"
+
 
 export default function Header() {
     const path = useLocation().pathname;
@@ -11,7 +14,7 @@ export default function Header() {
     return (
         <Navbar className='border-b-2'>
             <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-                <span className='px-3 py-1 bg-gradient-to-r from-blue-500 via-teal-400 to-cyan-300 rounded-lg text-white'>Erdem's</span>
+                <span className='px-3 py-1.5 bg-gradient-to-r from-blue-500 via-teal-400 to-cyan-400 rounded-lg text-white'>Erdem's</span>
                 Tour
             </Link>
             <form>
@@ -22,16 +25,22 @@ export default function Header() {
                     className='hidden lg:inline'
                 />
             </form>
-            <Button className='w-12 h-10 lg:hidden' color='gray' pill>
-                <AiOutlineSearch />
+            <Button className='w-13 h-11 lg:hidden' color='gray' pill>
+                <AiOutlineSearch className='w-4 h-6' />
             </Button>
-            <div className='flex gap-2 md:order-2'>
+            <div className='flex gap-1 md:order-2'>
+                <Button className='w-13 h-11  hidden sm:inline' color='gray' pill>
+                    <MdLanguage className='w-4 h-4' />
+                </Button>
+                <Button className='w-13 h-11  hidden sm:inline' color='gray' pill>
+                    <GrCurrency className='w-4 h-4' />
+                </Button>
                 <Button className='w-13 h-11  hidden sm:inline' color='gray' pill>
                     <FaMoon />
                 </Button>
                 <Link to="/sign-in">
                     <button class="relative inline-flex items-center justify-center p-0.5 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-                        <span class="relative px-6 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                             Sign In
                         </span>
                     </button>
