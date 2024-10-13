6d4ac13cbe0778e330d37ed87b93812babede469
Erdem Eşki <erdemeski00@gmail.com>
2024-10-13 21:10:51 +0300

    signup request structure

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 
 mongoose
@@ -15,9 +16,12 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
 
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
