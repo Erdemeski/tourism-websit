97083511f39b2664a3a6c8c6c03340e8ad29a961
Erdem Eşki <erdemeski00@gmail.com>
2024-10-13 14:59:29 +0300

    backend life cycle

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
+import userRoutes from './routes/user.route.js';
 
 
 mongoose
@@ -17,3 +18,6 @@ const app = express();
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
+
+
+app.use('/api/user', userRoutes);
