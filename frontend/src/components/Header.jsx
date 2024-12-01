2c3ba65b78a1dda919008cd21a070a1e307a3352
Erdem Eşki <erdemeski00@gmail.com>
2024-12-01 22:14:30 +0300

    initialing for localization structure (language and currency)

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,13 +2,19 @@ import { Avatar, Button, Dropdown, DropdownHeader, Modal, Navbar, NavbarToggle, 
 import { Link, useLocation } from 'react-router-dom'
 import React, { useState } from 'react'
 import { AiOutlineSearch } from 'react-icons/ai'
-import { FaMoon, FaSun } from 'react-icons/fa'
+import { FaMoon, FaSun, FaPoundSign } from 'react-icons/fa'
+import { FaTurkishLiraSign } from 'react-icons/fa6'
 import { GrCurrency } from "react-icons/gr"
 import { IoLanguage } from "react-icons/io5"
 import { useSelector, useDispatch } from "react-redux"
 import { toggleTheme } from '../redux/theme/themeSlice'
 import { signoutSuccess } from "../redux/user/userSlice";
 import { HiOutlineExclamationCircle } from 'react-icons/hi'
+import en from "../assets/lang_Icons/en.png";
+import tr from "../assets/lang_Icons/tr.png";
+import { useTranslation } from "react-i18next";
+//import '../i18n.jsx';
+
 
 
 
@@ -19,6 +25,20 @@ export default function Header() {
     const { currentUser } = useSelector(state => state.user);
     const { theme } = useSelector((state) => state.theme);
     const [showSignout, setShowSignout] = useState(false);
+
+    const [lang, setLang] = useState('en')
+    const [curr, setCurr] = useState('gbp')
+    console.log(lang)
+
+    const { t, i18n } = useTranslation()
+    const changeLanguage = (lng) => {
+        //i18n.changeLanguage(lng);
+        setLang(lng);
+        console.log(lng);
+    }
+
+
+
 
 
     const handleSignout = async () => {
@@ -58,12 +78,46 @@ export default function Header() {
                     <AiOutlineSearch className='w-4 h-6' />
                 </Button>
                 <div className='flex gap-1 md:order-2'>
-                    <Button className='w-13 h-11  hidden sm:inline' color='gray' pill>
-                        <IoLanguage className='w-4 h-4' />
-                    </Button>
-                    <Button className='w-13 h-11  hidden sm:inline' color='gray' pill>
-                        <GrCurrency className='w-4 h-4' />
-                    </Button>
+
+                    <Dropdown className='hidden sm:inline' label="" dismissOnClick={false} renderTrigger={() => <span>
+                        <Button className='w-13 h-11' color='gray' pill>
+                            <IoLanguage className='mt-0.5 w-4 h-4 flex items-center' />
+                        </Button>
+                    </span>}>
+                        <Dropdown.Item active={lang == 'en'} onClick={() => changeLanguage('en')} >
+                            <div className='flex justify-center items-center'>
+                                <img src={en} alt="" className='w-6 h-6 mr-2' />
+                                <span className='flex justify-center'>English</span>
+                            </div>
+                        </Dropdown.Item>
+                        <Dropdown.Item onClick={() => changeLanguage('tr')} >
+                            <div className='flex justify-center items-center'>
+                                <img src={tr} alt="" className='w-6 h-6 mr-2' />
+                                <span className='flex justify-center'>Türkçe</span>
+                            </div>
+                        </Dropdown.Item>
+                    </Dropdown>
+
+                    <Dropdown className='hidden sm:inline' label="" dismissOnClick={false} renderTrigger={() => <span>
+                        <Button className='w-13 h-11  hidden sm:inline' color='gray' pill>
+                            <GrCurrency className='w-4 h-4' />
+                        </Button>
+                    </span>}>
+                        <Dropdown.Item>
+                            <div className='flex justify-center items-center'>
+                                <FaPoundSign className='w-6 h-6 mr-1' />
+                                <span className='flex justify-center'>GBP</span>
+                            </div>
+                        </Dropdown.Item>
+                        <Dropdown.Item>
+                            <div className='flex justify-center items-center'>
+                                <FaTurkishLiraSign className='w-6 h-6 mr-1' />
+                                <span className='flex justify-center'>TRY</span>
+                            </div>
+                        </Dropdown.Item>
+                    </Dropdown>
+
+
                     <Button className='w-13 h-11  hidden sm:inline' color='gray' pill onClick={() => dispatch(toggleTheme())}>
                         {theme === 'light' ? <FaSun /> : <FaMoon />}
                     </Button>
